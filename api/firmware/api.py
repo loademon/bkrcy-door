9f4e38e8840e3026d0f214369129cfcc504082e3
loademon <loademon@tutanota.com>
2023-11-20 16:57:12 +0300

    test

--- api/firmware/api.py
+++ api/firmware/api.py
@@ -4,13 +4,6 @@ from app import app, asgi_app, jsonify
 from error import Error
 
 
-@app.errorhandler(Error)
-async def handle_error(error: Error):
-    response = jsonify({"error": error.message})
-    response.status_code = 404
-    return response
-
-
 @app.route("/user/<uid>", methods=["GET"])
 async def get_user(uid):
     id = f"user:{uid}"
--- api/firmware/app.py
+++ api/firmware/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
+
 app = Flask(__name__)
 
 from asgiref.wsgi import WsgiToAsgi
+
 asgi_app = WsgiToAsgi(app)
