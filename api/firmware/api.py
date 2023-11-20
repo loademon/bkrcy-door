6b559ed355a3a12f369e4c24600cb2c3ab3ade98
loademon <loademon@tutanota.com>
2023-11-20 16:05:15 +0300

    design for GET Request

--- api/firmware/api.py
+++ api/firmware/api.py
@@ -1,3 +1,21 @@
-from flask import Flask
+from db import rd
+from app import app, jsonify
+from error import Error
 
-app = Flask(__name__)
+
+@app.errorhandler(Error)
+def handle_error(error: Error):
+    response = jsonify({"error": error.message})
+    response.status_code = 404
+    return response
+
+@app.route("/user/<uid>", methods=["GET"])
+async def get_user(uid):
+    id = f"user:{uid}"
+    if await rd.exists(id):
+        user = await rd.hgetall(id)
+        return user
+    raise Error(f"User {uid} not found")
+
+
+app.run(debug=True, threaded=True)
