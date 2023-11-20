f9216354150935e9b5884333d906ad74a2f77624
loademon <loademon@tutanota.com>
2023-11-20 20:02:57 +0300

    adding how to use page

--- api/firmware/api.py
+++ api/firmware/api.py
@@ -3,11 +3,19 @@ from app import app, asgi_app, jsonify
 
 from error import Error
 
+@app.route("/user", methods=["GET"])
+# how to use page
+async def user():
+    return "Usage: /user/<id>"
+
 
 @app.route("/user/<uid>", methods=["GET"])
+# getting full user info by id
 async def get_user(uid):
     id = f"user:{uid}"
     if await rd.exists(id):
-        user = await rd.hgetall(id)
-        return user
+        return await rd.hgetall(id)
     raise Error(f"User {uid} not found")
+
+
+app.run(port=8000)
