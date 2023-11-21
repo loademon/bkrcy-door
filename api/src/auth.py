8f2b0ad6b000101839c3c56b176feb58dd81839f
loademon <loademon@tutanota.com>
2023-11-21 16:53:09 +0300

    Add User Auth.
    This commit introduces a user authentication system along with a test-protected route for our web application. The integration uses Flask-Login for managing user sessions and implements asynchronous database calls to Redis for retrieving user data.
    
    Key features and updates include:
    - Implementation of the User class with an asynchronous get method for fetching user data from Redis.
    - Creation of a user login functionality, including an HTML form on the '/login' route, which handles POST requests to authenticate users.
    - Utilization of password hashing and verification using Werkzeug's security methods.
    - Introduction of a test-protected route '/protected' to demonstrate the use of the @login_required decorator. This route is currently for testing purposes and will be enhanced with functional features in future updates.
    - Implementation of a logout functionality on the '/logout' route, allowing users to end their sessions.
    
    This update provides a solid foundation for secure user authentication and session management. The '/protected' route serves as a placeholder and will be developed into a fully functional component of the application in subsequent updates.

---
+++ api/src/auth.py
@@ -0,0 +1,77 @@
+from app import (
+    app,
+    LoginManager,
+    UserMixin,
+    redirect,
+    url_for,
+    request,
+    login_user,
+    logout_user,
+    login_required,
+    check_password_hash,
+    generate_password_hash,
+    render_template_string,
+    current_user,
+)
+from db import rd
+from secrets import token_hex
+import asyncio
+
+app.secret_key = token_hex(16)
+
+login_menager = LoginManager()
+login_menager.init_app(app)
+
+
+class User(UserMixin):
+    def __init__(self, id, password):
+        self.id = id
+        self.password = password
+
+    async def get(id):
+        passw = await rd.hget(f"account:{id}", "password")
+        return User(id, passw)
+
+
+@login_menager.user_loader
+def user_loader(id):
+    return User(id, asyncio.run(User.get(id)))
+
+
+@app.route("/login", methods=["GET"])
+async def login():
+    # create simple login page with button and form
+    page = """
+    <form action="/login" method="POST">
+        <input type="text" name="id">
+        <input type="password" name="password">
+        <input type="submit" value="login">
+    </form>
+    """
+    return page
+
+
+@app.post("/login")
+async def login_post():
+    id = request.form["id"]
+    user = await User.get(id=id)
+
+    if user is None or not check_password_hash(user.password, request.form["password"]):
+        return redirect(url_for("login"))
+
+    login_user(user)
+    return redirect(url_for("protected"))
+
+
+@app.route("/protected")
+@login_required
+def protected():
+    return render_template_string(
+        "Logged in as: {{ user.id }}", user=current_user
+    )
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return "Logged out"
