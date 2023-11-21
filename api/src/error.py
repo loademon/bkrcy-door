3fb046365c7a11493494887075b2f7229a7f8c71
loademon <loademon@tutanota.com>
2023-11-21 16:59:37 +0300

    Implement Custom Error
    This commit introduces a custom error handling mechanism to our Flask web application. The implementation enhances the robustness of the application by providing a structured response to errors, improving the overall user experience in encountering exceptions.
    
    Major Changes:
    - Definition of a custom `Error` class, serving as a base class for exceptions in the application. This class includes an initialization method that accepts an error message.
    - Implementation of an error handler (`@app.errorhandler`) specifically for the custom `Error` class. This handler is designed to catch exceptions of type `Error` and return a structured JSON response.
    - The error handler is set to return a JSON response containing the error message and sets the HTTP status code to 404, indicating that the requested resource is not found.
    
    Key Benefits:
    - This custom error handling provides a clear and consistent method for responding to various errors throughout the application.
    - It enhances the API's professionalism and user-friendliness, as users receive a well-defined JSON response in case of errors.
    - The use of HTTP status code 404 in the error response aids in the proper interpretation of the issue by client applications or users.

---
+++ api/src/error.py
@@ -0,0 +1,17 @@
+from app import app, jsonify
+
+
+class Error(Exception):
+    """Base class for exceptions"""
+
+    def __init__(self, msg: str) -> None:
+        self.message = msg
+
+
+@app.errorhandler(Error)
+async def handle_error(error: Error):
+    response = jsonify({"error": error.message})
+    response.status_code = 404
+    return response
+
+print("Error handler initialized")
