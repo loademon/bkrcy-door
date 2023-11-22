1f2b186090194a1964982edd05bcf06472b70596
loademon <loademon@tutanota.com>
2023-11-23 02:52:58 +0300

    Import ifadeleri yeniden düzenlendi ve WsgiToAsgi ile uygulama başlatıldı
    - app.py dosyasındaki import ifadeleri daha iyi okunabilirlik ve bakım kolaylığı için yeniden düzenlendi.
    - İlgili importlar gruplandırıldı ve kullanılmayanlar kaldırıldı.
    - ASGI uyumluluğu sağlamak için WsgiToAsgi entegre edildi.
    - Uygulamanın başlatılmasını bildiren print ifadesi standart hale getirildi.

--- api/src/app.py
+++ api/src/app.py
@@ -1,5 +1,23 @@
-from flask import Flask, jsonify, request, redirect, url_for, render_template_string
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from flask import (
+    Flask,
+    jsonify,
+    request,
+    redirect,
+    url_for,
+    render_template_string,
+    flash,
+    get_flashed_messages,
+    render_template,
+)
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    login_required,
+    logout_user,
+    current_user,
+)
+from markupsafe import Markup
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
