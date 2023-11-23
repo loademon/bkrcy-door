a8c73a0753a5be6a23c4ade1253b7dc17595e791
loademon <loademon@tutanota.com>
2023-11-23 03:05:24 +0300

    Comprehensive Enhancements in API, User Registration, Login UI, and Flash Messaging Functionality
    Bu commit, API, kullanıcı kayıt formu, giriş sayfası ve flash mesaj işlevselliği üzerinde kapsamlı iyileştirmeler içermektedir. Her bir değişiklik, uygulamanın kullanıcı dostuluğunu ve teknik etkinliğini artırmak için tasarlanmıştır.
    
    1. Kullanıcı Kayıt Formu (user_form.html) İyileştirmeleri:
       - Kullanıcı bilgilerini (ID, ad, okul numarası, bitiş tarihi) girmek için metin alanları eklenmiştir.
       - Dinamik olarak oluşturulan kapı erişim seçenekleri, kullanıcılara kapı erişimi vermek için kullanılır.
       - JavaScript kullanılarak geliştirilen etkileşimli kapı arama özelliği, kullanıcılara arama yaparak istedikleri kapıyı hızlıca bulma imkanı sunar.
       - Kapı seçeneklerinin etkileşimli kontrolü için JavaScript fonksiyonları eklenmiştir.
       - Form doğrulama fonksiyonu, kullanıcının en az bir kapı seçeneğini işaretlemesini zorunlu kılar.
       - Tasarım, modern ve kullanıcı dostu bir arayüz sunmak için CSS ile stilize edilmiştir.
    
    2. Giriş Sayfası (Login HTML) Değişiklikleri:
       - Giriş sayfası tasarımı yenilenmiş, daha sade ve modern bir görünüm kazandırılmıştır.
       - Kullanıcı adı ve şifre için iki ayrı giriş alanı bulunur, her biri net ve anlaşılır etiketlere sahiptir.
       - Giriş yapma butonu, kullanıcıların giriş bilgilerini göndermelerini sağlar.
       - Buton ve giriş alanları, kullanıcı deneyimini iyileştirmek için CSS ile uyumlu bir şekilde stilize edilmiştir.
    
    3. Flash Mesajları (Flash.html) Geliştirmeleri:
       - Flash mesajlar için CSS ile görsel tasarım iyileştirmeleri yapılmıştır. Mesajlar, kullanıcıya bilgi verirken estetik bir görünüm sunar.
       - Flash mesajların yavaşça açılması ve kapatılması için JavaScript ile animasyonlar eklenmiştir.
       - Etkileşimli kapanma butonu, kullanıcının mesajı kapatmasına izin verir.
       - Sayfa yüklemesi sırasında flash mesajların otomatik olarak açılması için JavaScript fonksiyonları eklenmiştir.
    
    4. API (api.py) Güncellemeleri:
       - 'json' modülünden 'dumps' ve 'loads' işlevleri eklenerek JSON veri işleme yetenekleri genişletilmiştir.
       - Yeni HTTP yöntemleri tanımlanmıştır (GET, POST), bu sayede sunucunun farklı türde isteklere yanıt vermesi sağlanır.
       - Kullanıcı oluşturma ve bilgi alma işlevleri için yeni rota işlevleri eklenmiştir.
       - Kullanıcı bilgileri Redis veritabanına kaydedilirken, kapı erişim bilgileri JSON formatında saklanır.
    
    Bu güncellemeler, kullanıcıların uygulamayı daha etkin ve zevkli bir şekilde kullanabilmelerine olanak tanırken, aynı zamanda geliştiricilere daha verimli ve yönetilebilir bir kod tabanı sunmaktadır.
    
    doors değişkenini liste olarak almak ve liste olarak pushlamak için json.dumps ve json.loads fonksiyonları kullanılır.

--- api/src/api.py
+++ api/src/api.py
@@ -1,28 +1,77 @@
 import auth
+from json import dumps, loads
 from db import rd
-from app import app, asgi_app
+from app import (
+    app,
+    request,
+    asgi_app,
+    login_required,
+    flash,
+    redirect,
+    url_for,
+    get_flashed_messages,
+    render_template,
+    render_template_string,
+    Markup,
+)
 from error import Error
 
 
 @app.route("/", methods=["GET"])
-# how to use page
 def home():
     go_to_login_page_button = "<a href='/login'>Go to login page</a>"
     return f"<h1>usage: /user, /user/{{uid}}, /login</h1>{go_to_login_page_button}"
 
 
 @app.route("/user", methods=["GET"])
-# how to use page
+@login_required
 def user():
-    return "<h1>usage: /user/{uid}</h1>"
+    available_doors = rd.smembers("doors")
+    flash_messages_html = render_template("flash.html")
+    flash_messages_html += "".join(
+        [
+            f'<div class="alert">{message}<span class="closebtn" onclick="closeFlashMessage(event)">&times;</span></div>'
+            for message in get_flashed_messages()
+        ]
+    )
+    page = ""
+    for door in available_doors:
+        page += f'<div class="checkbox-container">' \
+            f'<label for="{door}">' \
+            f'<input type="checkbox" name="door_access" value="{door}">{door}' \
+            f'</label>' \
+            f'</div>\n'
+    page = Markup(page)
+
+    return render_template("user_form.html", doors=page, flash_messages= flash_messages_html)
 
 
 @app.route("/user/<uid>", methods=["GET"])
-# getting full user info by id
 def get_user(uid):
     id = f"user:{uid}"
     if rd.exists(id):
-        return rd.hgetall(id)
+        data = rd.hgetall(id)
+        data["doors"] = loads(data["doors"])
+        return data
     raise Error(f"User {uid} not found")
 
-#deneme
+
+@app.route("/user", methods=["POST"])
+def post_user():
+    data = request.form
+
+    rd.hset(
+        name=f"user:{data['uid']}",
+        mapping={
+            "name": data["name"],
+            "school_number": data["school_number"],
+            "expiration_date": data["expiration_date"],
+            "doors": dumps(data.getlist("door_access")),
+        },
+    )
+    flash("User created successfully", "success")
+
+    return redirect(url_for("user"))
+
+
+app.run()
